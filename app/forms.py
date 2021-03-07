7558fdac872546868a0866478c0c83b6f9750646
Ben Castan <ben@castanfamily.com>
2021-03-07 17:09:09 +1100

    Part 3

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtfforms import Stringfield, PasswordField, BooleanField, SubmitField
-from wtfforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
-“class LoginForm(FlaskForm):
+
+class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
