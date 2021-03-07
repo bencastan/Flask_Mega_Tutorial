2e171b63eefba1c65d77df9d47e33b8d2af4bfd4
Ben Castan <ben@castanfamily.com>
2021-03-07 15:57:21 +1100

    2nd lesson pt2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index ():
-    return "Hello world!"
+def index():
+    user = {'username': 'Ben'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Sydney'
+        },
+        {
+            'author': {'username': 'Lauren'},
+            'body': 'The avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    # return render_template('index.html', user=user, posts=posts)
