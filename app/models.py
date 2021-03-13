a9e099c0fcd16ed6c70b07c3527534788f0591b0
Ben Castan <ben@castanfamily.com>
2021-03-13 12:35:51 +1100

    Chapter4 Completed

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
